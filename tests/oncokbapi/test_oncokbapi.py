c455bd56c727bb47152daa3c8f3758eeb0a970a0
Mark J Chen <mjchen.gene@gmail.com>
2024-11-05 15:07:16 -0500

    feat: get auth

--- tests/oncokbapi/test_oncokbapi.py
+++ tests/oncokbapi/test_oncokbapi.py
@@ -3,12 +3,14 @@
 import unittest
 
 from pyoncokb.oncokbapi import OncokbApi
+from tests.testconfig import TestConfig
 
+config = TestConfig()
 
 class OncokbApiTestCase(unittest.TestCase):
     """Test OncokbApi class."""
 
     def test_init(self):
         """Test initialization of OncokbApi instance."""
-        oncokb_api = OncokbApi(auth="MOCKED")
+        oncokb_api = OncokbApi(auth=config.get_oncokb_authorization())
         self.assertTrue(isinstance(oncokb_api, OncokbApi))
